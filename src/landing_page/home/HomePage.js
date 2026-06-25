4f65f9b268c1b433d805555349e84e94e4718106
UDAY <udaygujjula1@gmail.com>
2026-06-25 13:30:57 +0530

    Routingdone

--- src/landing_page/home/HomePage.js
+++ src/landing_page/home/HomePage.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import Hero from './Hero';
-import Awards from './Awards';
-import Pricing from './Pricing';
-import Education from './Education';
-import Stats from './Stats';
-import OpenAccount from '../OpenAccount';
-import Footer from '../Footer';
-import Navbar  from '../Navbar';
-function Homepage() {
-    return ( 
-        <>
-        <Navbar/>
-        <Hero/>
-        <Awards/>
-        <Stats/>
-        <Pricing/>
-        <Education/>
-        <OpenAccount/>
-        <Footer/>
-        </>
-     );
+import React from "react";
+import Hero from "./Hero";
+import Awards from "./Awards";
+import Stats from "./Stats";
+import Pricing from "./Pricing";
+import Education from "./Education";
+
+import OpenAccount from "../OpenAccount";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+
+function HomePage() {
+  return (
+    <>
+      <Hero />
+      <Awards />
+      <Stats />
+      <Pricing />
+      <Education />
+      <OpenAccount />
+    </>
+  );
 }
 
-export default Homepage;
+export default HomePage;
